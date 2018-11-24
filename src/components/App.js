f1946295ae29b5e368ffeb6866651be8d5265c5d
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2018-11-24 20:50:13 +0100

    add subpages 🍫

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,94 @@ import React, { Component } from 'react';
 import Button from './Button';
 
 import logo from '../assets/logo.png';
+import hackyeah from '../assets/hackyeah.png';
+
 import './app.sass';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      button: 0
+    }
+  }
+
+  handleClick = prop => {
+    this.setState({ button: prop })
+  }
+
   render() {
+    const { button } = this.state
+
     return (
       <div className="raknroll">
         <header className="header">
           <img src={logo} className="logo" alt="logo" />
         </header>
+
         <main className="main">
-          <p className="title">
-            DZIĘKUJEMY ZA TWOJE WARKOCZE! ABY SPRWDŹCIIĆ, CZY DO NAS DOTARŁY WYBIERZ SPOSÓB W JAKI BYŁY PRZEKAZANE.
-          </p>
+          {button === 0 ?
+            <div>
+              <p className="paragraph">
+                DZIĘKUJEMY ZA TWOJE WARKOCZE! ABY SPRAWDZIĆ, CZY DO NAS DOTARŁY WYBIERZ SPOSÓB W JAKI BYŁY PRZEKAZANE
+              </p>
+              <div className="button-container">
+                <Button title="WYSYŁAŁEM SAMODZIELNIE" handleClick={() => this.handleClick(1)} />
+                <Button title="PRZEKAZANE SALONOWI" handleClick={() => this.handleClick(2)} />
+              </div>
+            </div>
+            : null}
 
-          <div className="button-container">
-            <Button title="WYSYŁAŁEM SAMODZIELNIE" />
-            <Button title="PRZEKAZANE SALONOWI" />
-          </div>
+          {button === 1
+            ?
+            <div className="option-container">
+              <p className="paragrap-2">
+                WPISZ ADRES EMAIL, KTÓRY PODAŁEŚ/PODAŁAŚ W OŚWIADCZENIU
+              </p>
 
-          {/* <form className="form">
-            <label>Name:
-              <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
-          </form> */}
+              <form className="form">
+                <label>
+                  <input
+                    placeholder="EMAIL"
+                    type="text"
+                    name="name"
+                    className="input"
+                  />
+                </label>
+                <Button title="WYŚLIJ" />
+              </form>
+            </div>
+            : null
+          }
+
+          {button === 2
+            ?
+            <div className="option-container">
+              <p className="paragrap-2">
+                JEŚLI ODDAWAŁEŚ WŁOSY W CIĄGU OSTATNICH DWÓCH MIESIĘCY, SKONTAKTUJ SIĘ Z SALONEM, ABY UPEWNIĆ SIĘ ZE WARKOCZE ZOSTAŁY WYSŁANE
+              </p>
+              <p className="paragrap-2">
+                JEŚLI TEN CZAS MINĄŁ, WPISZ ADRES EMAIL, KTÓRY PODAŁEŚ/PODAŁAŚ W OŚWIADCZENIU
+              </p>
+
+              <form className="form">
+                <label>
+                  <input
+                    placeholder="EMAIL"
+                    type="text"
+                    name="name"
+                    className="input"
+                  />
+                </label>
+                <Button title="WYŚLIJ" />
+              </form>
+            </div>
+            : null
+          }
         </main>
+        <footer className="footer">
+          <img src={hackyeah} className="footer__logo" alt="hackyeah" />
+        </footer>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,25 +3,14 @@ import React from 'react'
 import './button.sass';
 
 export default class Button extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    this.setState({ value: event.target.value });
-    alert('You clicked: ' + this.props.title);
-  }
-
   render() {
     return (
       <input
-      className="button"
+        className="button"
         type="button"
         value={this.props.title}
-        onClick={this.handleClick}
-        />
+        onClick={this.props.handleClick}
+      />
     );
   }
 }
