85a89f08296f288acda78557df33c5adaa04e90b
krzysztofjedynak <jedynakpoczta@gmail.com>
2018-11-24 22:41:57 +0100

    Download pdf file

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+const API = "https://boiling-everglades-94066.herokuapp.com/thankyou?firstName=";
 const UserRow = props => {
       return (
         <tr>
           <td>{props.user.name} twoje włosy są juz u nas!</td>
-          <td><a href={props.user.link}>Kliknij w link</a></td>
+          <td><a href={API + props.user.name}>Kliknij w link</a></td>
         </tr>
       );
   };
